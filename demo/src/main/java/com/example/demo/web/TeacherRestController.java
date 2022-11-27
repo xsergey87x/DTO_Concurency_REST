b7e992c04d80ff6604d6a870ba74150b18559cba
Tytyanchuk Sergey <xsergey87x@ukr.net>
2022-11-27 23:54:13 +0200

    c2

--- demo/src/main/java/com/example/demo/web/TeacherRestController.java
+++ demo/src/main/java/com/example/demo/web/TeacherRestController.java
@@ -1,21 +1,26 @@
 package com.example.demo.web;
 
+import com.example.demo.DTO.entityDto.TeacherDTO;
+import com.example.demo.DTO.serviceDto.Mapper;
 import com.example.demo.entity.Teacher;
 import com.example.demo.service.TeacherService;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value = "/teacher")
 public class TeacherRestController {
 
 private final TeacherService teacherService;
+private final Mapper mapper;
 
-    public TeacherRestController(TeacherService teacherService) {
-        this.teacherService = teacherService;
-    }
+  public TeacherRestController(TeacherService teacherService, Mapper mapper) {
+    this.teacherService = teacherService;
+    this.mapper = mapper;
+  }
 
   @PostMapping(value = "/saveTeacher")
   public Teacher addTeacher(@RequestBody Teacher teacher)
@@ -30,20 +35,26 @@ private final TeacherService teacherService;
   }
 
   @GetMapping(value = "/getAll")
-  public List<Teacher> getAllTeacher()
+  public List<Teacher> getAllTeacher()  {return teacherService.getAllTeacher();}
+
+  @GetMapping(value = "/getAllDto")
+  public List<TeacherDTO> getAllDtoTeacher()  {return teacherService.getAllTeacher().stream().map(mapper::mapEntityToDto).collect(Collectors.toList());}
+
+  @GetMapping(value = "/getById")
+  public Optional getAllTeacher(@RequestParam Long id)
   {
-    return teacherService.getAllTeacher();
+    return teacherService.getTeacherById(id);
+  }
+
+  @GetMapping(value = "/getDtoById")
+  public TeacherDTO getAllDtoTeacher(@RequestParam Long id)
+  {
+    return mapper.mapEntityToDto((Teacher)teacherService.getTeacherById(id).get());
   }
 
   @PostMapping(value = "/saveTestTeacher")
   public Teacher createTestTeacher()
   {
     return teacherService.createTeacher(new Teacher("Jordan","Peterson","male",40));
-  }
-
-  @GetMapping(value = "/getAllById")
-  public Optional getAllTeacher(@RequestParam Long id)
-  {
-    return teacherService.getTeacherById(id);
   }
 }
